6a576983d2e48643fc39434a0826618dac55d30c
Bharath <marambharath7@gmail.com>
2023-12-27 20:38:59 +0530

    added 2nd script

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh: Hello Surech, Good Morning"
+echo "Surech: Hi Ramesh, very Good Morning"
+echo "Ramesh: How are u Suresh?"
+echo "Suresh: I am Good Ramesh, How are you?"
+echo "Ramesh: I am Great"
