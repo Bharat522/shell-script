2c15804dd711a3af4a6dcc87a4f7bc14966593f4
Bharath <marambharath7@gmail.com>
2023-12-29 13:59:21 +0530

    passing arugments values to the variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-PERSON1=$1
+PERSON1=$1 # here PERSON1 means varibale and $1 means value passed to that variable
 PERSON2=$2
 
 echo "$PERSON1: Hello $2, Good Morning"
 echo "$PERSON2: Hi $1, very Good Morning"
 echo "$PERSON1: How are you $2?"
 echo "$PERSON2: I am good $1, How are you"
+
+# at the time of o/p we need to pass the values
+# without passing values this script has no meaningfull
