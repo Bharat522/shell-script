cbe3d8dcbb510ea527edc4ba3018b315b86c0541
Bharath <marambharath7@gmail.com>
2023-12-29 15:10:26 +0530

    varible values are entered in the script

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,6 +3,8 @@
 PERSON1=Bharath
 PERSON2=Ganesh
 
+# variables values are entered inside the script ex: bharath & Ganesh are values to varible person1 & person2
+
 echo "$PERSON1: Hello $PERSON2, Good Morning"
 echo "$PERSON2: Hi $PERSON1, very Good Morning"
 echo "$PERSON1: How are u $PERSON2?"
