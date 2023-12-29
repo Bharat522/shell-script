efd0487eae0eb352d9d1bdcf13cd2caab90a3c3a
Bharath <marambharath7@gmail.com>
2023-12-29 15:07:04 +0530

    username & password program

--- 05-variables.sh
+++ 05-variables.sh
@@ -7,8 +7,6 @@ echo "Please enter your username:"  # what ever we write in echo it will be prin
 
 read -s USERNAME # read command is used to just read the value entered for USERNAME and (-s) is used to hide the value entered in o/p screen
 
-read -i USERNAME # use TEXT as the initial text for Readline
-
 echo "Username: $USERNAME" # this is used to show our username/password in the o/p screen, usually uasername/password are confidential so they will not be disclosed on any screen, this is only used to practice purpose only
 
 echo "Please enter your password:"
