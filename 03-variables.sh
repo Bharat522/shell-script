ff786f15641f717097e6fcbf70ff66c56fc808a9
Bharath <marambharath7@gmail.com>
2023-12-29 15:13:01 +0530

    value executed at runtime and assigned to the varible DATE

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,3 +3,5 @@
 DATE=$(date) # DATE is variable and $(date) is value and without giving date in brackets this program wont execute
 
 echo "script executed date & time: $DATE" 
+
+# here in this script we have executed the value date in run time and that value will be assinged to DATE varible.
