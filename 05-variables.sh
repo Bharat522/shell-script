9ce78af9b2bbfdcc083b8852e39c51110986dc58
Bharath <marambharath7@gmail.com>
2023-12-29 13:44:43 +0530

    username and password with variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Please enter your username:"
+echo "Please enter your username:"  # what ever we write in echo it will be printed in o/p screen
 
-read -s USERNAME
+read -s USERNAME # read command is used to just read the value entered for USERNAME and (-s) is used to hide the value entered in o/p screen
 
-username=$USERNAME
+username=$USERNAME # this means small letter username is variables and $USERNAME is value what we pass at the time of output
 
 echo "Please enter your password:"
 
