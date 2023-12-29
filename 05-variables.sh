41bb54743f9573d269aab0c89ee1f8220bb9c738
Bharath <marambharath7@gmail.com>
2023-12-29 13:48:57 +0530

    username and password with variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -6,7 +6,7 @@ read -s USERNAME # read command is used to just read the value entered for USERN
 
 username=$USERNAME # this means small letter username is variables and $USERNAME is value what we pass at the time of output
 
-echo "Username: $USERNAME"
+echo "Username: $USERNAME" # this is used to show our username/password in the o/p screen, usually uasername/password are confidential so they will not be disclosed on any screen, this is only used to practice purpose only
 
 echo "Please enter your password:"
 
