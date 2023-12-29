455d6b66daf3c0941b593fcf18597a00f7a13f98
Bharath <marambharath7@gmail.com>
2023-12-29 13:55:51 +0530

    passing arugments values to the variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1: Hello $PERSON2, Good Morning"
-echo "$PERSON2: Hi $PERSON1, very Good Morning"
-echo "$PERSON1: How are you $PERSON2?"
-echo "$PERSON2: I am good $PERSON1, How are you"
+echo "$PERSON1: Hello $1, Good Morning"
+echo "$PERSON2: Hi $1, very Good Morning"
+echo "$PERSON1: How are you $2?"
+echo "$PERSON2: I am good $1, How are you"
