cb1ee2b53b668f601c6f5dd3d097868d84b23f92
Bharath <marambharath7@gmail.com>
2024-01-04 14:48:05 +0530

    install mysql

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -6,6 +6,7 @@ if [ $ID -ne 0 ]
 then
     echo "ERROR:: please run this script with root user access"
     exit 1 # this is exit status if you dont run thhis script as root user script will stop here at this point as id will not be 0
+    # exit status value must be otherthan zero as the ID for ROOT user is 0
 else
     echo "you are root user"
 fi 
@@ -13,3 +14,11 @@ fi
     # if user is root we will proceed and run the next step 
     # if not root user then error msg will get displayed
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing MySQL is failed"
+    exit 1
+else
+    echo "Installing MySQL is SUCCESS"
+fi
