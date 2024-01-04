0fade0b121361671d83933f6c98fe082976a0a07
Bharath <marambharath7@gmail.com>
2024-01-04 14:50:24 +0530

    install mysql

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -5,15 +5,15 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROR:: please run this script with root user access"
-    #exit 1 # this is exit status if you dont run thhis script as root user script will stop here at this point as id will not be 0
-    # exit status value must be otherthan zero as the ID for ROOT user is 0
+    exit 1 # this is exit status if you dont run thhis script as root user script will stop here at this point as id will not be 0
+    # if exit status value must be otherthan zero as the ID for ROOT user is 0
 else
     echo "you are root user"
 fi 
     # else is not required in this statement as we are checking the user is root or not
     # if user is root we will proceed and run the next step 
     # if not root user then error msg will get displayed
-yum install mysql -y
+yum install mysqll -y
 
 if [ $? -ne 0 ]
 then
