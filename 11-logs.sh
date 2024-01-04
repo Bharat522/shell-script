5e66f71488c110dff3ab7b2d83fa2812bdb406a7
Bharath <marambharath7@gmail.com>
2024-01-04 19:45:29 +0530

    logs

--- 11-logs.sh
+++ 11-logs.sh
@@ -25,7 +25,7 @@ then
     echo -e " $R ERROR:: Please run this script with root access $N"
     exit 1 
 else
-    echo -e  "$G You are root user $N"
+    echo -e  " You are $G root $N user"
 fi 
 
 yum install mysql -y &>> $LOGFILE
