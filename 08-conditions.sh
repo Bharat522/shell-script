7dff6280bbb27e9538fd4c128909be959fc5f5c5
Bharath <marambharath7@gmail.com>
2024-01-04 12:22:49 +0530

    if condition given number is grater or not

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1 # this means we are explicitly giving the number/value at the time of 
 
 if [ $NUMBER -gt 100 ] # for operators in shell please check this link  https://www.tutorialspoint.com/unix/unix-basic-operators.htm
 then
-   echo "Given number $NUMBER is less than 100"
+   echo "Given number $NUMBER is greater than 100"
 else
-   echo "Given number $NUMBER is not greater than 100"
+   echo "Given number $NUMBER is less than 100"
 fi
