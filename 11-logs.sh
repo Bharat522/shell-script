9f931cb0cefce67aeeccb9a48a9aebc6e59bdba5
Bharath <marambharath7@gmail.com>
2024-01-04 19:38:32 +0530

    logs

--- 11-logs.sh
+++ 11-logs.sh
@@ -2,22 +2,27 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S) # %F: year-month-date, %H: hour, %M: minute, %S: second 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){  
     if [ $1 -ne 0 ]   
     then 
-        echo "ERROR:: $2 Failed" 
+        echo -e "ERROR:: $2  $R Failed $N"       # echo is for printing purpose and for help command:   "man echo"
         exit 1
     else
-        echo "$2 SUCCESS" 
+        echo -e "$2  $G SUCCESS $N" 
     fi
 }
 
 if [ $ID -ne 0 ] 
 then
-    echo "ERROR:: Please run this script with root access"
+    echo -e " $R ERROR:: Please run this script with root access $N"
     exit 1 
 else
     echo "You are root user"
