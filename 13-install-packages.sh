96e0d299d754ba04d94fb36a66a2eb5e5677662c
Bharath <marambharath7@gmail.com>
2024-01-05 16:23:57 +0530

    installing packges by passing args at the time of executing

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -37,7 +37,7 @@ fi
 for package in $@
 do
     yum list installed $package &>> $LOGFILE #check installed or not
-    if [$? -ne 0 ]   #if not installed
+    if [ $? -ne 0 ]   #if not installed
     then
         yum install $package -y &>> $LOGFILE   # install the package
         VALIDATE $? "Installation of $package" #validate
